a6174fde5b2a1f9b542f94a189667cc7b75cc459
lidingke <lidingke@hust.edu.cn>
2016-07-07 18:54:19 +0800

    counter area

--- cvc.py
+++ cvc.py
@@ -27,10 +27,9 @@ class findCircles(object):
 
     def method(self):
         img = self.img
-        # img = self._edgeDetectDiff(img)
-        img = self._edgeDetectCanny(img)
+        img = self._edgeDetectDiff(img)
+        # img = self._edgeDetectCanny(img)
         cv2.imshow("adaptiveThreshold",img)
-        # cv2.imwrite('threshold.jpg',img)
         cv2.waitKey(0)
         # img = self._circleFind(img)
         img = self._circleFindContours(img)
@@ -40,7 +39,7 @@ class findCircles(object):
 
     def _edgeDetectCanny(self,img):
         # img = cv2.GaussianBlur(img,(3, 3 ),0)
-        img = cv2.Canny(img, 10, 150)
+        img = cv2.Canny(img, 10, 200)
         return img
 
     def _edgeDetectDiff(self,img):
@@ -48,7 +47,7 @@ class findCircles(object):
         img  = cv2.pyrMeanShiftFiltering(img,5,5,1)
         img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-        img = cv2.GaussianBlur(img,(3, 3 ),0)
+        # img = cv2.GaussianBlur(img,(3, 3 ),0)
         # kernel = np.ones((10,10),np.uint8)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3, 3))
         # pdb.set_trace()
@@ -58,9 +57,10 @@ class findCircles(object):
         # img = erosion
         # arrimg = cv2.cv.fromarray(img)
         erode = cv2.erode(img,kernel)
-        dilate = cv2.dilate(img,kernel)
+        # dilate = cv2.dilate(img,kernel)
         # img = np.array(arrimg)
-        img = cv2.absdiff(dilate,erode)
+        # img = cv2.absdiff(dilate,erode)
+        img = cv2.absdiff(img,erode)
         img = cv2.bitwise_not(img)
         img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41, 3)
 
@@ -79,12 +79,17 @@ class findCircles(object):
         contours, hierarchy = cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
         result = np.ones(img.shape)*255
         for x in contours:
-            # pdb.set_trace()
-            cv2.drawContours(result,x,-1,(0,0,255))
+
+            area = cv2.contourArea(x)
+            cvMom = cv2.moments(x)
+            if cvMom['m00'] != 0.0:
+                cvMomXY = (cvMom['m10']/cvMom['m00'],cvMom['m01']/cvMom['m00'])
+                circleIndex = self._isCircle(area, cvMomXY, x)
+                print "area" , area , "circleIndex:", circleIndex,"mom:", cvMomXY
+                if circleIndex > 0.8:
+                    cv2.drawContours(result, x, -1, (0,0,255))
         cv2.imshow("img", result)
         cv2.waitKey(0)
-
-
 
     def _circleFind(self,img):
         para = 150
@@ -180,6 +185,18 @@ class findCircles(object):
         cv2.imwrite('result.jpg',self.origin)
         return img
 
+
+    def _isCircle(self, area, center, contours):
+        # pdb.set_trace()
+        forMaxNumber = []
+        for x in contours:
+            absLen = ((center[0]-x[0][0])**2 + (center[1]-x[0][1])**2)**0.5
+            forMaxNumber.append(absLen)
+        forMaxNumber.sort()
+        maxLen = forMaxNumber[-1]
+        if maxLen == 0:
+            return 0
+        return area/(maxLen**2*3.14)
 
 import sys
 import os
